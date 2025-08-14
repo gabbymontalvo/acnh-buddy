083245de67b0eb5248541c95c960edab1fde1923
gabbymontalvo <gabriella.montalvo2006@gmail.com>
2025-08-14 19:17:02 -0400

    webscraped bugs, need to figure out png images

--- backend/data/insects.py
+++ backend/data/insects.py
@@ -13,3 +13,21 @@ soup = get_soup(url)
 # test
 print(soup)
 
+bug_table = soup.find('table', class_= ['sortable', 'jquery-tablesorter'])
+
+bugs = []
+
+for row in bug_table.find_all('tr')[1:]:
+    cells = row.find_all('td')
+    name = cells[1].get_text(strip = True)
+    # webscrape logo here
+    price = cells[3].get_text(strip = True)
+
+    bugs.append({
+        'name': name,
+        'sell_price': price
+    })
+
+# test
+for buggies in bugs:
+    print(buggies)
