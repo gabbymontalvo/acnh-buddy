653e54eb170c816b6632bd74623a7d07ffe295b4
gabbymontalvo <gabriella.montalvo2006@gmail.com>
2025-08-14 20:06:23 -0400

    fixed bells and added image url to insects

--- backend/data/insects.py
+++ backend/data/insects.py
@@ -20,11 +20,17 @@ bugs = []
 for row in bug_table.find_all('tr')[1:]:
     cells = row.find_all('td')
     name = cells[1].get_text(strip = True)
-    # webscrape logo here
-    price = cells[3].get_text(strip = True)
+    
+    img_tag = cells[2].find('img')
+    img_url = None
+    if img_tag and 'src' in img_tag.attrs:
+        img_url = img_tag['src']
+
+    price = cells[3].get_text(strip = True).replace('Bells', ' Bells')
 
     bugs.append({
         'name': name,
+        'image_url': img_url,
         'sell_price': price
     })
 
